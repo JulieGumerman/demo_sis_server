39d503b2b0d8b3655941e257fb2ea4bf29aad643
JulieGumerman <julie.gumerman@gmail.com>
2022-05-09 19:00:39 -0600

    sufficient data

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const fs = require('fs')
 const server = express();
 
 const fake_data = fs.readFileSync('./fake_user_data.json', 'utf-8')
+const fake_balances_data = fs.readFileSync('./fake_balances_data.json', 'utf-8')
 
 server.get('/',(req, res) => {
-    res.send({message: "Hello world!!!", food: "nom nom nom", user_data: JSON.parse(fake_data)})
+    res.send({message: "Hello world!!!",})
+})
+
+server.get('/academic_background/',(req, res) => {
+    res.send({user_data: JSON.parse(fake_data)})
+})
+
+server.get('/balances/',(req, res) => {
+    res.send({ user_data: JSON.parse(fake_balances_data)})
 })
 
 server.listen(3333, () => {
